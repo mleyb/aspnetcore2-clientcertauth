a29ea5011141072ce2285c2384a8046a26da1f25
Mark Leybourne <mleyb@live.co.uk>
2017-09-15 18:11:09 +0100

    Initial commit

---
+++ Auth.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace aspnetcore2_clientcertauth
+{
+    public class CustomAuthenticationOptions : AuthenticationSchemeOptions  
+    {
+        // Authentication options properties
+    }
+
+    public class CustomAuthenticationHandler : AuthenticationHandler<CustomAuthenticationOptions>  
+    {
+        public CustomAuthenticationHandler(IOptionsMonitor<CustomAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock) { }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+                return Task.FromResult(
+                    AuthenticateResult.Success(
+                        new AuthenticationTicket(
+                            new ClaimsPrincipal(),
+                            new AuthenticationProperties(),
+                            this.Scheme.Name)));
+        }
+    }
+
+    public static class CustomAuthenticationExtensions  
+    {
+        public static AuthenticationBuilder AddCustomAuthentication(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<CustomAuthenticationOptions> configureOptions)
+        {
+            return builder.AddScheme<CustomAuthenticationOptions, CustomAuthenticationHandler>(authenticationScheme, displayName, configureOptions);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace aspnetcore2_clientcertauth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                // the scheme name has to match the value we're going to use in AuthenticationBuilder.AddScheme(...)
+                options.DefaultAuthenticateScheme = "Custom Scheme";
+                options.DefaultChallengeScheme = "Custom Scheme";
+            })
+            .AddCustomAuthentication(
+                "Custom Scheme",
+                "Custom Scheme",
+                o => { });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
